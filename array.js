ecbefbbd7f237369499b3720ce8ee04c6d178998
p232001 <61044192+p232001@users.noreply.github.com>
2021-05-28 12:18:38 +0530

    initial commit

---
+++ array.js
@@ -0,0 +1,10 @@
+var arr = ['kl.txt', 'M4', 'hello.txt', 'N7'];
+
+var files = [];
+var folders = [];
+
+arr.forEach(function(val) {
+    if (indexOf(arr) === -1);
+    console.log('folder');
+    else console.log('files');
+})
